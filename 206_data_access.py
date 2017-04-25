656b513a371cbf26c0aba907b3f338591b8f1cf7
Emma Aboukasm <eabouk@umich.edu>
2017-04-25 19:40:28 -0400

    working through bugs

--- 206_data_access.py
+++ 206_data_access.py
@@ -27,9 +27,7 @@ except:
 ## Go through each state, find the park data data and cache it. 
 ## You should start by finding out the link to one state's listed parks and format a for loop to gather the data from each state, 
 ## and cache it into a cache file. Each state should have a simple HTML string that lists all the parks and data about each state. 
-
-#First I need a dict of states and other zones that parks reside so that I can loop through in order to put the data in the cache 
-# dictionary. 
+ 
 state_abrv = {}
 state_abrv = {"Alabama": "al", "Alaska": "ak", "Arizona": "az", "Arkansas": "ar","California": "ca", "Colorado": "co", "Connecticut": "ct",
 "Delaware": "de", "Florida": "fl", "Georgia": "ga", "Hawaii": "hi", "Idaho": "id", "Illinois": "il", "Indiana": "in", "Iowa": "ia", 
@@ -37,10 +35,9 @@ state_abrv = {"Alabama": "al", "Alaska": "ak", "Arizona": "az", "Arkansas": "ar"
 "Minnesota": "mn", "Mississippi": "ms", "Missouri": "mo", "Montana": "mt", "Nebraska": "ne", "Nevada": "nv", "New Hampshire": "nh", 
 "New Jersey": "nj", "New Mexico": "nm", "New York": "ny", "North Carolina": "nc", "North Dakota": "nd", "Ohio": "oh", "Oklahoma": "ok", 
 "Oregon": "or", "Pennsylvania": "pa", "Rhode Island": "ri", "South Carolina": "sc", "South Dakota": "sd", "Tennessee": "tn", "Texas": "tx",
-"Utah": "ut", "Vermont": "vt", "Virginia": "va", "Washington": "wa", "West Virginia": "wv", "Wisconsin": "wi", "Wyoming": "wy", 
-"Northern Mariana Islands": "mp", "Guam": "gu", "Puerto Rico": "pr", "Virgin Islands": "vi"}
-#after writing the data_cache function, I will loop through the values of the dictionary above in order to put stuff in a cache file. 
-#final_list = []
+"Utah": "ut", "Vermont": "vt", "Virginia": "va", "Washington": "wa", "West Virginia": "wv", "Wisconsin": "wi", "Wyoming": "wy"}
+
+#state_abrv holds all the states and their respective abbreviation so that it can loop through HTML pages in the function below
 final_html = []
 def get_national_park_data():
 	
@@ -75,6 +72,7 @@ def get_national_park_data():
 	
 
 all_html_pages = get_national_park_data()
+print('printing number of pages', len(all_html_pages))
 
 
 ## STEP 3: Define a function that gathers HTML data representing all the front page articles on the NPS website. Do some searching
@@ -101,25 +99,25 @@ def get_frontpage_articles():
 	return nps_front_page
 
 all_front_articles = get_frontpage_articles()
-#print(type(all_front_articles))
+
 
 ############### PART TWO ############### 
 ## Class 1 ##
 
 ## STEP 1: Define a class NationalPark that accepts HTML formatted string as input and uses BeautifulSoup data parsing to create
 ## instance variables for every instance of the National Park within the constructor. 
-# def get_park_names(html_doc):
 
 
 class NationalPark(object):
 
 ## STEP 2: Define class instance variables within the constructor. 
-## Your class will have 3 instance variables:
-## 		-park_name: the name of the park
+## Your class will have 8 instance variables:
+## 		-park_name: the name of a single park
 ## 		-park_location: a string containing the state(s)/city of the park
 ## 		-park_description:  a string containing a description about the park
 ##		-park_type: the type of the park (battle ground, national park, historical landmark etc.)
-
+##		NOTE: park_names, park_locations, park_descriptions and park_types are all lists of all the instance variables for each 
+##			HTML page
 	def __init__(self, html_doc):
 		self.soup = BeautifulSoup(html_doc, "html.parser")
 		self.page_info = self.soup.find_all("div",{"class": "col-md-9 col-sm-9 col-xs-12 table-cell list_left"})
@@ -159,9 +157,6 @@ class NationalPark(object):
 				self.park_type = p.text
 				self.park_types.append(p.text)
 
-		#state_name
-		# for g in page_info:
-		# 	state_name = 
 ## STEP 3: Define a method of your class called num_parks that finds and returns the number of parks in each state. It will take in 
 ## HTML data representing each state's park page and returns the number of parks in that state.
 
@@ -176,8 +171,8 @@ class NationalPark(object):
 				for x in num_parks:
 					#print(x.text)
 					num_parks = self.soup.find_all("strong")
-					num_parks = num_parks[0].text
-		return num_parks
+					number_parks = num_parks[0].text
+		return number_parks
 
 ## STEP 4: Define a method of your class called total_revenue that takes HTML data and finds how much revenue each state will get 
 ## every year from their state parks.
@@ -191,8 +186,8 @@ class NationalPark(object):
 				for x in revenue:
 					#print(x.text)
 					revenue = self.soup.find_all("strong")
-					revenue = revenue[2].text
-		return revenue
+					revenue_num = revenue[2].text
+		return revenue_num
 
 ## STEP 5: Define a method of your class called visitor_score that takes HTML data representing each state's park page and returns 
 ## the number of visitors that visit national parks annually. This will not affect any data, simply it finds the number of annual
@@ -213,10 +208,7 @@ class NationalPark(object):
 
 
 one_park = NationalPark(all_html_pages[0])
-# print (one_park.total_revenue())
-# print (one_park.visitor_score())
-# print (one_park.num_parks())
-# print (one_park.park_location)
+
 
 ############### PART THREE ############### 
 ## Class 2 ##
@@ -237,13 +229,13 @@ class Article(object):
 			article_title = p.find_all("h3",{"class":"Feature-title carrot-end"})
 			for r in article_title:
 				self.title = r.text
-			# print(article_title.text)
-
-
+			
 		for p in self.page_info:
 			description = p.find_all("p", {"class": "Feature-description"})
 			for f in description:
 				self.description = f.text
+
+## STEP 3: Define a method of this class called list_of_titles that will gather a list of titles for all the front page articles
 
 	def list_of_titles(self):
 		self.list_of_titles = []
@@ -254,6 +246,9 @@ class Article(object):
 				self.list_of_titles.append(r.text)
 		
 		return self.list_of_titles
+
+## STEP 4: Define a method of this class called list_of_descriptions that will gather a list of subheaders for all the front page 
+## articles
 
 	def list_of_descriptions(self):
 		self.list_of_descriptions = []
@@ -281,6 +276,7 @@ article_thing = Article(all_front_articles)
 states_parks = {}
 for x in state_abrv:
 	states_parks[x] = CACHE_DICTION[x]
+
 #states_parks is a dictionary that contains a state name and the respective html for that state. It's just easier to create a new
 #dictionary out of the CACHE_DICTION so that we can easily make lists of things. 
 all_nat_park_info = []
@@ -321,7 +317,6 @@ def find_temp_data():
 		soup = BeautifulSoup(html_doc, 'html.parser')
 
 		find_temps = soup.find_all("table", {"class": "articletable tablecol-1-left"})
-		print(len(find_temps))
 
 		for f in find_temps:
 			state_find = f.find_all('tbody')
@@ -335,7 +330,7 @@ def find_temp_data():
 					state_ave_temp = state_info[1].text
 			
 					STATE_TEMP[state_name] = state_ave_temp
-					print(len(STATE_TEMP))
+
 		
 		CACHE_DICTION["STATE_TEMP"] = STATE_TEMP
 		f = open(CACHE_FILE, 'w')
@@ -347,24 +342,85 @@ def find_temp_data():
 	else:
 		return CACHE_DICTION["STATE_TEMP"]
 
-print (find_temp_data())
-print (len(CACHE_DICTION["STATE_TEMP"]))
-print(sorted(CACHE_DICTION["STATE_TEMP"]))
+#print (find_temp_data())
+temp_data = find_temp_data()
+# print(type(temp_data))
+new_temp_data = sorted(temp_data.items(), key = lambda x: x[0])
+# print(type(new_temp_data))
+# #print(new_temp_data)
+states, temp_values = zip(*new_temp_data)
+print("printing states and temp values", states, temp_values)
+#print("PRINTING NEW TEMP DATA", new_temp_data.values())
+#print (len(CACHE_DICTION["STATE_TEMP"]))
+#print(sorted(CACHE_DICTION["STATE_TEMP"]))
+#state_temps = sorted(CACHE_DICTION['STATE_TEMP'].values())
+
 ## STEP 4: Write code to make lists of all the data that will be inputted into the state's table in your database. You will need 
 ## a list of state_abrv, number of parks, revenue for each state, and avg_temp for each state. 
 all_state_abrv = state_abrv.values()
 
-number_all_states_parks = []
-num_revenue_all_states = []
-# for state in all_html_pages:
-# 	one_state = NationalPark(state)
-#  	revenue = one_state.total_revenue()
-#  	num_revenue_all_states.append(revenue)
 
-#  	number_parks = one_state.num_parks()
-#  	number_all_states_parks.append(number_parks)
+def get_state_table_info():
+	state_table_tups = []
+	iterated_list_states = []
+	if "STATE_TABLE_TUPS" not in CACHE_DICTION:
 
-#state_table_tups = zip(state_abrv.values(), number_all_states_parks, num_revenue_all_states, sorted(CACHE_DICTION["STATE_TEMP"].values()))
+		number_all_states_parks = []
+		num_revenue_all_states = []
+		for state in sorted(states_parks):
+			#print(states_parks[state])
+			one_state = NationalPark(states_parks[state])
+			#before this said sorted(states_parks.keys()) and then states_parks[state] for the NationalPark(state)...
+			revenue = one_state.total_revenue()
+			#print("PRINTING REVENUE", revenue)
+			# num_revenue_all_states.append(revenue)
+
+
+			# print("caching...", state)
+			number_parks = one_state.num_parks()
+			# print("num parks...", number_parks)
+			# print("num revenue", revenue)
+			# #print("num revenue...", num_revenue_all_states)
+			# number_all_states_parks.append(number_parks)
+			
+			# one_tup = 
+
+			state_table_tups.append(zip(state_abrv[state], number_parks, revenue, CACHE_DICTION['STATE_TEMP'][state]))
+
+		for tup in state_table_tups:
+			iterated_list_states.append(tup)
+			print(tup)
+				
+		# state_table_zip = itertools.zip_longest(state_abrv.values(), number_all_states_parks, num_revenue_all_states, temp_values)
+
+
+		# for item in state_table_zip:
+		# 	state_table_tups.append(item)
+		# 	print("PRINTING STATE TUP", item)
+
+		CACHE_DICTION["STATE_TABLE_TUPS"] = iterated_list_states
+
+		f = open(CACHE_FILE, 'w')
+		f.write(json.dumps(CACHE_DICTION))
+		f.close()
+
+		return CACHE_DICTION["STATE_TABLE_TUPS"]
+
+	else: 
+		return CACHE_DICTION["STATE_TABLE_TUPS"]
+
+all_state_table = get_state_table_info()
+print (sorted(all_state_table))
+print(len(all_state_table))
+
+new_state_list = []
+for tup in all_state_table:
+	new_state_list.append(tup)
+print(new_state_list)
+
+## THINGS TO FIX:
+	#- data is in states table but its off... not sure how much off but its not right
+	#-check the articles table... see if its accurate. 
 ############### PART FIVE ############### 
 ## Database Work ##
 
@@ -395,11 +451,15 @@ cur.execute(statement)
 ## For the states table:
 	# -state_abrv: text primary key (2 letter abbreviation)
 	# -num_parks: integer 
-	# -revenue: integer (or long?)
-statement = "CREATE TABLE IF NOT EXISTS states (state_abrv TEXT primary key, num_parks INTEGER, revenue INTEGER, avg_temp INTEGER)"
+	# -revenue: text
+	# -avg_temp: Real (float)
+statement = "CREATE TABLE IF NOT EXISTS states (state_abrv TEXT primary key, num_parks INTEGER, revenue TEXT, avg_temp REAL)"
 cur.execute(statement)
 #load data below...
-statement = 'INSERT OR IGNORE INTO states VALUES(?, ?, ?)'
+statement = 'INSERT OR IGNORE INTO states VALUES(?, ?, ?, ?)'
+for tup in new_state_list:
+	cur.execute(statement, tup)
+conn.commit()
 #Will finish loading this in when I gather average temp data 
 
 
@@ -426,13 +486,18 @@ conn.close()
 ############### PART SIX ############### 
 ## Queries ##
 
-## STEP 1: Write a query to join the articles table on the parks table, type_park (on parks table) with related_park (on articles)
-## This query will find the number of articles that have specific parks listed within them.
+## STEP 1: Write a query to join on the states table the abrv column with the park_location on the parks table
+query = "SELECT states.state_abrv, park.park_location FROM parks INNER JOIN states on state.state_abrv = park.park_location"
+cur.execute(query)
+joined_result = cur.fetchall()
 
 ## STEP 2: Write a query to find the states with num_parks greater than 10. Save that into a variable titled most_parks. 
+query = "SELECT num_parks FROM states WHERE states.num_parks > 10"
+cur.execute(query)
+most_parks = cur.fetchall()
 
-## STEP 3: Write a query to find the top 5 states with the highest revenue. Save that into a variable titled most_revenue.
-
+## STEP 3: Write a query to find the states with the hottest temperature. Save that into a variable titled hottest_temp.
+query = "SELECT avg_temp FROM states WHERE states.avg_temp > 60"
 
 
 ############### PART SEVEN ############### 
@@ -496,9 +561,15 @@ class TEST_PART_3(unittest.TestCase):
 
 class TEST_PART_4(unittest.TestCase): 
 	def test_new_dict(self):
-		self.assertEqual(len(states_parks), 54)
-	# def test_tups(self):
-	# 	self.assertEqual(iterated_nat_park[0], type((4, 5)))
+		self.assertEqual(len(states_parks), 50)
+	def test_temp_dict(self):
+	 	self.assertEqual(len(CACHE_DICTION["STATE_TEMP"]), 50)
+	def test_get_state_info(self):
+		self.assertEqual(type(get_state_table_info()), type([]))
+	def test_get_state_info_2(self):
+		self.assertEqual(len(get_state_table_info()), 50)
+	def test_get_state_info_3(self):
+		self.assertEqual(type(all_state_table[0]), type((3, 4)))
 # #will write more test cases when more code is written....
 # #will solidify the test case ideas when more code is written...
 # ## Remember to invoke all your tests...
